13cf44e5571a9cea2ccd257e5b1d4fa3a7881bf7
Jason Yang <didwotjs120@gmail.com>
2023-11-01 13:43:17 +0900

    3. defined header and insert header's own status to object

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 const spreadsheetContainer = document.querySelector('#spreadsheet-container');
 const btnExportingSpreadsheet = document.querySelector('#export-btn');
 const sellectedCellInfo = document.querySelector('#cell-status');
-const numOfRows = 10;
-const numOfCols = 10;
+const numOfRows = 15;
+const numOfCols = 15;
 const spreadsheetArray = [];
+const alphabetArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 // 3. 문자열이 아닌 객체 데이터 생성하기
 class Cell {
-    constructor(row, col, cellName, cellData, isHeader) {
+    constructor(row, col, cellName, cellData, isHeader, disabled) {
         this.row = row;
         this.col = col;
         this.cellName = cellName;
         this.cellData = cellData;
         this.isHeader = isHeader;
+        this.disabled = false;
     }
 }
-creatTable();
+createTable();
 
-function creatTable(){
+function createTable(){
     for (i = 0; i < numOfRows; i++){
         const rowList = []
         for(j = 0; j < numOfCols; j++){
             const cellName = `${i}-${j}`;
             const cell = new Cell(i, j, cellName, "", false);
+            if(i === 0) {
+                cell.isHeader = true;
+                cell.cellData = alphabetArray[j-1];
+                cell.disabled = true;
+            }
+            if(j === 0) {
+                cell.isHeader = true;
+                cell.cellData = i;
+                cell.disabled = true;
+            }
+            if(i === 0 && j === 0){
+                cell.cellData = null;
+            }
             rowList.push(cell);
         }
         spreadsheetArray.push(rowList);
     }
-    // console.log(spreadsheetArray);
+    console.log(spreadsheetArray);
     creatCellElement();
 };
 
@@ -36,8 +51,19 @@ function creatCellElement(){
         rowContainer.classList.add('row-container');
         for(j = 0; j < spreadsheetArray[0].length; j++) {
             const cellElement = document.createElement('input');
+            const cell = spreadsheetArray[i][j];
             cellElement.classList.add('cell');
+            cellElement.id = cell.cellName;
+            cellElement.value = cell.cellData;
+            cellElement.disabled = cell.disabled;
+
+            if(cell.isHeader) {
+                cellElement.classList.add('header');
+            }
+
             rowContainer.append(cellElement);
+
+            
         }
         spreadsheetContainer.append(rowContainer);
     }
